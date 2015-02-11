725067677e6180eeaea9dee8dd219cc1032307ef
Parth <parth.savjani@iiitb.org>
2015-02-12 00:31:18 +0530

    php files

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Spandan 2015 Login</title>
+</head>
+
+<body>
+<h2>Login Here</h2>
+<form action="login_submit.php" method="post">
+<fieldset>
+<p>
+<label for="username">Username</label>
+<input type="text" id="username" name="username" value="" maxlength="20" />
+</p>
+<p>
+<label for="password">Password</label>
+<input type="text" id="password" name="password" value="" maxlength="20" />
+</p>
+<p>
+<input type="submit" value="Login" />
+</p>
+<a href="/signup.php">New User?..Signup</a>
+</fieldset>
+</form>
+</body>
+</html>
